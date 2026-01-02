ce7b3f7edd0cb0043c422ace4e5ed7418b72dbd4
NoohaFathima <noohaa.fathimaa143@gmail.com>
2026-01-02 22:11:54 +0530

    changes

--- login checker.py
+++ login checker.py
@@ -5,6 +5,6 @@ is_admin = True
 if is_admin:
     print('welcom,admin!')
 elif username == 'nooha' and password == 'star123':
-    print('WElcome,Nooha!')
+    print('Welcome,Nooha!')
 else:
     print('access denied')
